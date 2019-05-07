00e876803a9b9b4878f6db76f02121671718d64b
sumatrae <676119192@qq.com>
2019-05-07 14:37:08 +0800

    Add icon, modify UI

--- psu_viewer.py
+++ psu_viewer.py
@@ -1,17 +1,14 @@
-import pymeasure
-from pymeasure.instruments.keysight import KeysightN5767A
-import sys
-import re
-from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
-from PyQt5.QtGui import QPalette
-from PyQt5.uic import loadUi
-from PyQt5.QtCore import pyqtSlot, QTimer, Qt
 import configparser
+import gc
 import os
+import re
+import sys
 
-supplier = "Keysight"
-instrument_name = "N5767A"
-default_address = "GPIB0::1::INSTR"
+from pymeasure.instruments.keysight import KeysightN5767A
+from PyQt5.QtCore import Qt, QTimer, pyqtSlot
+from PyQt5.QtGui import QIcon, QPalette
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
+from PyQt5.uic import loadUi
 
 
 class HistoryConfig():
@@ -19,7 +16,7 @@ class HistoryConfig():
         self.config = configparser.ConfigParser()
         self.python_config_path = './psu.ini'
         if not os.path.exists(self.python_config_path):
-            self.config['HISTORY'] = {'address': default_address,
+            self.config['HISTORY'] = {'address': "GPIB0::1::INSTR",
                                       'voltage_range': 5.0,
                                       'current_range': 1.0}
             with open(self.python_config_path, "w+") as f:
@@ -44,6 +41,7 @@ class PSU_Viewer(QDialog):
         self.timer.timeout.connect(self.update)
 
         self.psu_viewer = loadUi("psu_viewer.ui", self)
+        self.psu_viewer.setWindowIcon(QIcon('psu.png'))
 
         self.config = HistoryConfig()
 
@@ -59,7 +57,7 @@ class PSU_Viewer(QDialog):
         self.psu_viewer.lineEdit_voltage.setText(str(self.voltage_range))
         self.psu_viewer.lineEdit_current.setText(str(self.current_range))
 
-        self.psu_controller = PSU_Controller(supplier, instrument_name)
+        self.psu_controller = PSU_Controller()
         try:
             self.psu_controller.create_instance(self.address)
 
@@ -74,10 +72,6 @@ class PSU_Viewer(QDialog):
 
             self.update()
             self.timer.start(1000)
-            #
-            #
-            # self.timer.timeout.connect(self.update())
-            # self.timer.start(1000)
 
         except Exception as e:
             print(e)
@@ -89,33 +83,53 @@ class PSU_Viewer(QDialog):
         if not self.check_address_valid(address):
             QMessageBox.information(self, "Info",
                                     "Input format err",
-                                    QMessageBox.Yes | QMessageBox.No)
+                                    QMessageBox.Ok)
             return
 
-        self.address = address
-        self.psu_controller.create_instance(self.address)
-        self.model = self.psu_controller.psu.id
+        try:
+            self.set_default_view()
+            self.address = address
+            ret = self.psu_controller.create_instance(self.address)
+            if ret != True:
+                QMessageBox.information(self, "Info",
+                                        "Failed,please check address and physical connection",
+                                        QMessageBox.Ok)
+                return
 
-        self.psu_viewer.label_model_value.setText(self.model)
+            self.model = self.psu_controller.psu.id
+            if self.model:
+                self.psu_viewer.label_model_value.setText(self.model)
 
-        self.config.set("address", self.address)
+                self.config.set("address", self.address)
 
-        self.update()
-        self.timer.start(1000)
+                self.update()
+
+                self.timer.start(1000)
+
+                QMessageBox.information(self, "Info",
+                                        "Connect success",
+                                        QMessageBox.Ok)
+
+        except Exception as e:
+            QMessageBox.information(self, "Info",
+                                    "Failed,please check address and physical connection",
+                                    QMessageBox.Ok)
 
     @pyqtSlot()
     def on_pushButtonSwitch_clicked(self):
-        self.psu_enable_status = self.psu_controller.is_enabled()
-        if self.psu_enable_status:
-            self.psu_controller.psu.disable()
-            self.psu_viewer.pushButtonSwitch.setText("ON")
-            self.psu_viewer.label_status_value.setText("OUTPUT OFF")
-        else:
-            self.psu_controller.psu.enable()
-            self.psu_viewer.pushButtonSwitch.setText("OFF")
-            self.psu_viewer.label_status_value.setText("OUTPUT ON")
+        try:
+            self.psu_enable_status = self.psu_controller.is_enabled()
+            if self.psu_enable_status:
+                self.psu_controller.psu.disable()
 
-        self.update()
+            else:
+                self.psu_controller.psu.enable()
+
+            self.update()
+        except Exception as e:
+            QMessageBox.information(self, "Info",
+                                    "Failed,please connect psu first,{}".format(e),
+                                    QMessageBox.Ok)
 
     @pyqtSlot()
     def on_pushButtonSetVoltageCurrent_clicked(self):
@@ -123,7 +137,7 @@ class PSU_Viewer(QDialog):
         if not self.check_digital_valid(current_range):
             QMessageBox.information(self, "Info",
                                     "Input format err",
-                                    QMessageBox.Yes | QMessageBox.No)
+                                    QMessageBox.Ok)
 
             return
         self.current_range = current_range
@@ -132,20 +146,25 @@ class PSU_Viewer(QDialog):
         if not self.check_digital_valid(voltage_range):
             QMessageBox.information(self, "Info",
                                     "Input format err",
-                                    QMessageBox.Yes | QMessageBox.No)
+                                    QMessageBox.Ok)
             return
         self.voltage_range = voltage_range
 
-        self.psu_controller.set_voltage_range(float(self.voltage_range))
-        self.psu_controller.set_current_range(float(self.current_range))
+        try:
+            self.psu_controller.set_voltage_range(float(self.voltage_range))
+            self.psu_controller.set_current_range(float(self.current_range))
 
-        self.voltage_range = self.psu_controller.get_voltage_range()
-        self.current_range = self.psu_controller.get_current_range()
+            self.voltage_range = self.psu_controller.get_voltage_range()
+            self.current_range = self.psu_controller.get_current_range()
 
-        self.config.set("voltage_range", self.voltage_range)
-        self.config.set("current_range", self.current_range)
+            self.config.set("voltage_range", self.voltage_range)
+            self.config.set("current_range", self.current_range)
 
-        self.update()
+            self.update()
+        except Exception as e:
+            QMessageBox.information(self, "Info",
+                                    "Fail to set,{}".format(e),
+                                    QMessageBox.Ok)
 
     def get_lineEdit_address(self):
         address = self.psu_viewer.lineEdit_address.text()
@@ -172,44 +191,69 @@ class PSU_Viewer(QDialog):
         return bool(re.match(regx, i))
 
     def update(self):
-        self.model = self.psu_controller.get_model()
-        self.switch_status = self.psu_controller.is_enabled()
-        self.measurement_voltage = self.psu_controller.measure_voltage()
-        self.measurement_current = self.psu_controller.measure_current()
+        try:
+            self.model = self.psu_controller.get_model()
+            self.psu_viewer.label_model_value.setText(self.model)
+            if self.model:
+                self.psu_viewer.pushButtonSwitch.setEnabled(True)
+                self.psu_viewer.pushButtonSetVoltageCurrent.setEnabled(True)
+            else:
+                self.psu_viewer.pushButtonSwitch.setEnabled(False)
+                self.psu_viewer.pushButtonSetVoltageCurrent.setEnabled(False)
 
-        self.psu_viewer.label_model_value.setText(self.model)
-        self.psu_viewer.label_status_value.setText("ON" if self.switch_status else "OFF")
-        self.psu_viewer.label_measurement_voltage_value.setText(str(self.measurement_voltage))
-        self.psu_viewer.label_measurement_current_value.setText(str(self.measurement_current))
+            self.switch_status = self.psu_controller.is_enabled()
+            if self.switch_status:
+                self.psu_viewer.pushButtonSwitch.setText("off")
+                self.psu_viewer.label_status_value.setText("OUTPUT ON")
 
-        if self.switch_status:
-            self.psu_viewer.pushButtonSwitch.setText("OFF")
-            self.psu_viewer.label_status_value.setText("OUTPUT ON")
+                self.pe.setColor(QPalette.Window, Qt.green)
 
-            self.pe.setColor(QPalette.Window, Qt.green)
+            else:
+                self.psu_viewer.pushButtonSwitch.setText("on")
+                self.psu_viewer.label_status_value.setText("OUTPUT OFF")
 
-        else:
-            self.psu_viewer.pushButtonSwitch.setText("ON")
-            self.psu_viewer.label_status_value.setText("OUTPUT OFF")
+                self.pe.setColor(QPalette.Window, Qt.white)
 
-            self.pe.setColor(QPalette.Window, Qt.white)
+            self.psu_viewer.label_status_value.setPalette(self.pe)
 
+            self.measurement_voltage = self.psu_controller.measure_voltage()
+            self.measurement_current = self.psu_controller.measure_current()
+            self.psu_viewer.label_measurement_voltage_value.setText(str(self.measurement_voltage))
+            self.psu_viewer.label_measurement_current_value.setText(str(self.measurement_current))
+        except Exception as e:
+            pass
+
+    def set_default_view(self):
+        self.psu_viewer.label_model_value.setText("")
+
+        self.psu_viewer.pushButtonSwitch.setText("on")
+        self.psu_viewer.label_status_value.setText("OUTPUT OFF")
+
+        self.pe.setColor(QPalette.Window, Qt.white)
         self.psu_viewer.label_status_value.setPalette(self.pe)
 
-        # self.timer.start(1000)
+        self.psu_viewer.label_measurement_voltage_value.setText("")
+        self.psu_viewer.label_measurement_current_value.setText("")
+
+        self.psu_viewer.pushButtonSwitch.setEnabled(False)
+        self.psu_viewer.pushButtonSetVoltageCurrent.setEnabled(False)
 
 
 class PSU_Controller():
-    def __init__(self, supplier, instrument_name):
+    def __init__(self):
         self.psu = None
 
     def create_instance(self, address):
         del self.psu
 
+        gc.collect()
+
         self.psu = KeysightN5767A(address)
 
+        return self.check_connect_status()
+
     def get_model(self):
-        return " ".join(self.psu.id.split(",")[:2])
+        return ",".join(self.psu.id.split(",")[1::-1])
 
     def check_connect_status(self):
         return bool(self.psu.id)
